3a636e3946e2a41255e2aa8caa3e9c7b2ddcd415
javeedfazul <javeeed.tntj@gmail.com>
2022-03-11 20:16:23 +0530

    method

---
+++ src/test/java/com/git/SampleGit.java
@@ -0,0 +1,18 @@
+package com.git;
+
+public class SampleGit {
+
+	private void method1() {
+		System.out.println("m1");
+
+	}
+
+	private void method2() {
+		System.out.println("m2");
+
+	}
+
+	private void method3() {
+		System.out.println("m3");
+	}
+}
